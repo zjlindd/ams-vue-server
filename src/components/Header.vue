800f7337d25a099ec5cfc220f0b91744fbf321af
zjlindd <1685735773@qq.com>
2024-12-01 21:15:39 +0800

    网站搭建

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="header-wrap">
+        <div class="left">
+            <div class="logo"></div>
+        </div>
+        <div class="right">
+            <div class="menu"></div>
+        </div>
+    </div>
+</template>
