d462407b7afcb17af2973e0388638f8cf08a6b57
zjlindd <1685735773@qq.com>
2025-02-06 23:32:11 +0800

    文件夹名称修改

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="title">周军一是大坏蛋</div>
+  </div>
 </template>
 
 <style>
@@ -13,5 +15,14 @@ html, body {
   overflow: hidden;
   background: url("./assets/image/image.jpg") no-repeat center center;
   background-size: 100% 100%;
+}
+.title {
+  text-align: center;
+  font-size: 30px;
+  color: red;
+  font-weight: bold;
+  position: absolute;
+  top: 30%;
+  left: 44%;
 }
 </style>
